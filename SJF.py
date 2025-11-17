612973190a2f03782b7f523034d3cebe345454df
Samantha <tanakiswa4@gmail.com>
2025-11-17 11:59:25 +0400

    added user input to SJF code

--- SJF.py
+++ SJF.py
@@ -3,31 +3,59 @@ def sjf(process_list):
     gantt = []
     completed ={}
 
-    while process_list != []:
+    while process_list []:
         available = []
         for p in process_list:
             if p[1] <= t:
                 available.append(p)
-        if available == []:
+        if not available:
             t += 1
             gantt.append("Idle")
             continue
-        else:
-            available.sort()
+            available.sort(key=lambda x: x[0])
             process = available[0]
             burst_time = process[0]
-            pID = process[2]
             arrival_time = process[1]
+            pID = process[2]
+
             t += burst_time
-            gantt.append(pID)
             ct = t
             tt = ct - arrival_time
             wt = tt - burst_time
-            process_list.remove(process)
-            completed[pID] = [ct, tt, wt]
-    print(gantt)
-    print(completed)
 
-if __name__ == "__main__":
-    process_list=[]
-    sjf(process_list)
+            gantt.append(pID)
+            completed[pID] = [burst_time, wt, tt]
+
+            process_list.remove(process)
+
+return gantt, completed
+
+def main():
+    process_list = []
+
+    while True:
+        try:
+            n = int(input("Enter number of process: "))
+            if n <= 0:
+                print("Enter a positive integer")
+                continue
+                break
+        except ValueError:
+            print("Enter an integer")
+
+for i in range(1, n + 1):
+         while True:
+             try:
+                 bt = int(input(f"Enter burst time for P{i}: "))
+                if bt < 0:
+print("Enter a positive integer")
+continue
+except ValueError:
+                print("Enter a positive integer")
+continue
+
+arrival_time = 0
+pID = f"P{i}"
+
+process_list.append([bt, arrival_time, pID])
+gantt, completed = sjf(process_list)
